422ce6559f0a6476a53ebbfb0f10a49342388fe8
Indranil Bhuin <53374607+indra110@users.noreply.github.com>
2022-10-26 23:51:28 +0530

    animal sounds

---
+++ index.js
@@ -0,0 +1,34 @@
+document.querySelectorAll(".animal")[0].addEventListener("click", function() {
+  var audio = new Audio("sounds/dog.wav");
+  audio.play();
+})
+
+document.querySelectorAll(".animal")[1].addEventListener("click", function() {
+  var audio = new Audio("sounds/cat.wav");
+  audio.play();
+})
+
+document.querySelectorAll(".animal")[2].addEventListener("click", function() {
+  var audio = new Audio("sounds/lion.mp3");
+  audio.play();
+})
+
+document.querySelectorAll(".animal")[3].addEventListener("click", function() {
+  var audio = new Audio("sounds/eagle.mp3");
+  audio.play();
+})
+
+document.querySelectorAll(".animal")[4].addEventListener("click", function() {
+  var audio = new Audio("sounds/horse.mp3");
+  audio.play();
+})
+
+document.querySelectorAll(".animal")[5].addEventListener("click", function() {
+  var audio = new Audio("sounds/dragon.mp3");
+  audio.play();
+})
+
+document.querySelectorAll(".animal")[6].addEventListener("click", function() {
+  var audio = new Audio("sounds/cow.mp3");
+  audio.play();
+})
